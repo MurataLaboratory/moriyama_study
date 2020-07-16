361753060da38e462907b11dcc944ae511c88609
keimoriyama <keischwiiz@gmail.com>
2020-07-16 16:10:56 +0900

    最後の’’がきになる

--- morphological_analysis.py
+++ morphological_analysis.py
@@ -6,17 +6,19 @@ def split_word(word_list):
     return word_list.split(' ')
 
 path = "narrative-response_train.txt"
+
 list1, list2 = [], []
 with open(path, encoding= "utf-8")as f:
     while True:
         l = f.readline()
-        if not l:
+        if l:
+            # Tabについて分割
+            s = l.split('\t')
+            # 語りと応答について分割
+            list1.append(s[0])
+            # 謎に改行コードが入ったので消しておく
+            list2.append(s[1].rstrip('\n'))
+        else:
             break
-        # Tabについて分割
-        s = l.split('\t')
-        # 語りと応答について分割
-        list1.append(s[0])
-        # 謎に改行コードが入ったので消しておく
-        list2.append(s[1].rstrip('\n'))
 
-print(split_word(list1[0]))
+print(split_word(list1[3]))
